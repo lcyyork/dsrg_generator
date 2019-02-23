06df6b030ce8a36a3010ffd071006532494763c5
York <lcy@uga.edu>
2019-02-23 01:44:09 -0500

    modify mo_space

--- mo_space.py
+++ mo_space.py
@@ -1,15 +1,24 @@
 # spin-orbital space
 so_space = ['g', 'p', 'h', 'v', 'c', 'a']
-space_priority_so = {k: v for k, v in zip(so_space, range(6))}
+space_priority_so = {k: v for v, k in enumerate(so_space)}
 space_relation_so = {'c': {'c'}, 'a': {'a'}, 'v': {'v'},
                      'p': {'a', 'v'}, 'h': {'c', 'a'},
                      'g': {'c', 'a', 'v'}
                      }
 
+# spin-integrated alpha subspace
+space_priority_a = space_priority_so
+space_relation_a = space_relation_so
+
+# spin-integrated beta subspace
+mo_space_b = [i.upper() for i in so_space]
+space_priority_b = {k: v for v, k in enumerate(mo_space_b)}
+space_relation_b = {'C': {'C'}, 'A': {'A'}, 'V': {'V'},
+                    'P': {'A', 'V'}, 'H': {'C', 'A'},
+                    'G': {'C', 'A', 'V'}
+                    }
+
 # spin-integrated space
-mo_space = so_space + [i.upper() for i in so_space]
-space_priority = {k: v for k, v in zip(mo_space, range(12))}
-space_relation = {'c': {'c'}, 'a': {'a'}, 'v': {'v'}, 'C': {'C'}, 'A': {'A'}, 'V': {'V'},
-                  'p': {'a', 'v'}, 'h': {'c', 'a'}, 'P': {'A', 'V'}, 'H': {'C', 'A'},
-                  'g': {'c', 'a', 'v'}, 'G': {'C', 'A', 'V'}
-                  }
+mo_space = so_space + mo_space_b
+space_priority = {k: v for v, k in enumerate(mo_space)}
+space_relation = {**space_relation_a, **space_relation_b}

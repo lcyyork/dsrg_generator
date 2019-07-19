a6279bfccc6e72d84f8e4c208677d8d80439bc1f
York <lcy@uga.edu>
2019-07-18 23:27:30 -0400

    add method to perform ddca

--- Term.py
+++ Term.py
@@ -251,6 +251,37 @@ class Term:
 
             yield Term(list_of_tensors, sq_op, self.coeff * sign, False).canonicalize_sympy()
 
+    def make_ddca(self, max_core, max_virt, single_ref):
+        """
+        Apply distinguished diagonal component approximation to this term.
+        :param max_core: the max number of core indices kept
+        :param max_virt: the max number of virtual indices kept
+        :param single_ref: ignore active indices
+        :return: yield filtered terms
+        """
+        indices_0 = self.sq_op.indices
+
+        for spaces in product(*[space_relation[i.space] for i in indices_0]):
+            count = {s: 0 for s in space_relation_so.keys()}
+            for s in spaces:
+                count[s.lower()] += 1
+            if count['c'] > max_core or count['v'] > max_virt:
+                continue
+            if count['a'] != 0 and single_ref:
+                continue
+
+            replacement = {}
+            next_index_number = {i: 0 for i in self.next_index_number.keys()}
+            for index_0, space in zip(indices_0, spaces):
+                replacement[index_0] = self._generate_next_index(space, next_index_number)
+            for i in self.indices_set:
+                if i not in replacement:
+                    replacement[i] = self._generate_next_index(i.space, next_index_number)
+
+            sign, list_of_tensors, sq_op = self._relabel_indices(replacement)
+
+            yield Term(list_of_tensors, sq_op, self.coeff * sign, False).canonicalize_sympy()
+
     @staticmethod
     def format_coeff(value, form=None):
         fraction = Fraction(value).limit_denominator(1000000)

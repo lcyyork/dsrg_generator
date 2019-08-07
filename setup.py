ffad2b25bf43e265bcf08384a7b2f6e831bfbcf0
York <lcy@uga.edu>
2019-08-07 00:40:54 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='dsrg_generator',
+    author='Chenyang Li',
+    author_email='bjyork0822@gmail.com',
+    description='python script to generate DSRG equations',
+    version="0.3.1",
+    license='MIT',
+    python_requires='~=3.6',
+    packages=setuptools.find_packages()
+)
